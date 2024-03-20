f6a9f659c2080617c363d3558dbf0f8b967c4ed0
phamquanghuy08204 <124269780+phamquanghuy08204@users.noreply.github.com>
2024-03-20 09:32:37 +0700

    first commit

---
+++ Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTLONKY5.Models
+{
+    public class Account
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Bạn phải nhập vào User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Bạn phải nhập vào Password")]
+        public string Password { get; set; }
+
+        public int Role { get; set; }
+
+        public string Img { get; set; }
+    }
+}
---
+++ Models/QLDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace BTLONKY5.Models
+{
+    public class QLDBcontext : DbContext
+    {
+        public QLDBcontext(DbContextOptions<QLDBcontext> options): base(options) { }
+        public DbSet<Account> Accounts { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().ToTable("Account");
+        }
+    }   
+}
